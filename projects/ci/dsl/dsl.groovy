ad6663fec56b944c0423b6bb7bb164eb3383ed5e
Andrew Block <andy.block@gmail.com>
2016-07-18 22:59:02 -0400

    Refactored projects and automated the majority of the installation and configuration

---
+++ projects/ci/dsl/dsl.groovy
@@ -0,0 +1,40 @@
+def jobs = [
+    [ name: 'coolstore-app-pipeline', gitUrl: 'http://gogs:3000/gogs/coolstore-app', gitBranch: 'master', openShiftHost: 'kubernetes.default.svc.cluster.local', openShiftPort: "443", openShiftSourceProject: 'coolstore-bdd-dev', openShiftSourceApplication: 'coolstore-app', openShiftDestinationProject: 'coolstore-bdd-prod', openShiftDestinationApplication: 'coolstore-app'],
+    [ name: 'coolstore-test-harness-pipeline', gitUrl: 'http://gogs:3000/gogs/coolstore-test-harness.git', gitBranch: 'master', openShiftHost: 'kubernetes.default.svc.cluster.local', openShiftPort: "443", openShiftSourceProject: 'coolstore-bdd-dev', openShiftSourceApplication: 'coolstore-rules', openShiftDestinationProject: 'coolstore-bdd-prod', openShiftDestinationApplication: 'coolstore-rules', kieServer: 'http://coolstore-rules.coolstore-bdd-dev.svc.cluster.local:8080/kie-server/services/rest/server'],
+    [ name: 'coolstore-rules-pipeline', gitUrl: 'http://gogs:3000/gogs/coolstore-rules.git', gitBranch: 'deployments', openShiftHost: 'kubernetes.default.svc.cluster.local', openShiftPort: "443", openShiftSourceProject: 'coolstore-bdd-dev', openShiftSourceApplication: 'coolstore-rules', openShiftDestinationProject: 'coolstore-bdd-prod', openShiftDestinationApplication: 'coolstore-rules', kieServer: 'http://coolstore-rules.coolstore-bdd-dev.svc.cluster.local:8080/kie-server/services/rest/server']
+]
+
+jobs.each { job ->
+
+    workflowJob(job.name) {
+        parameters {
+            stringParam "OPENSHIFT_HOST",job.openShiftHost,"OpenShift Host"
+            stringParam "OPENSHIFT_PORT",job.openShiftPort, "OpenShift Port"
+            stringParam "OPENSHIFT_SOURCE_PROJECT",job.openShiftSourceProject, "OpenShift Source Project"
+            stringParam "OPENSHIFT_SOURCE_APPLICATION",job.openShiftSourceApplication, "OpenShift Source Application"
+            stringParam "OPENSHIFT_DESTINATION_PROJECT",job.openShiftDestinationProject, "OpenShift Destination Project"
+            stringParam "OPENSHIFT_DESTINATION_APPLICATION",job.openShiftDestinationApplication, "OpenShift Destination Application"
+
+            if(job.kieServer) {
+                stringParam "KIE_SERVER_URL",job.kieServer, "KIE Server URL"
+            }
+
+        }
+
+      definition {
+        cpsScm {
+            scm {
+                git {
+                    remote {
+                        url(job.gitUrl)
+                    }
+
+                    branch(job.gitBranch) 
+
+                }
+            }
+            scriptPath "Jenkinsfile"
+        }    
+      }
+    }
+}
